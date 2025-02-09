60191ba2cfd792e32a97437b2ec2935ab4f1caba
Akshay Makkar <akshaym1103@gmail.com>
2025-02-09 13:51:14 +0530

    feat: resolve import

--- src/main/java/org/cactro/data/cacheapi/config/RedisConfig.java
+++ src/main/java/org/cactro/data/cacheapi/config/RedisConfig.java
@@ -2,6 +2,7 @@ package org.cactro.data.cacheapi.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
